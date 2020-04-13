32ad226adaa2c9bb5732dd6cd42f7d854b0b03f4
akhmettolegen <52886803+akhmettolegen@users.noreply.github.com>
2020-04-13 16:24:30 +0600

    Update HBepay.php

--- src/HBepay.php
+++ src/HBepay.php
@@ -33,70 +33,65 @@ class HBepay
             $token_api_url = $prod_url;
         }
 
-
         $fields = [
             'grant_type'      => 'client_credentials', 
-            'scope'           => 'payment',
+            'scope'           => 'payment usermanagement',
             'client_id'       => $hbp_client_id,
             'client_secret'   => $hbp_client_secret,
             'invoiceID'       => $hbp_invoice_id,
             'amount'          => $hbp_amount,
-            'curency'         => $hbp_currency,
+            'currency'        => $hbp_currency,
             'terminal'        => $hbp_terminal,
-        ];
-    
-        // build query for request
-        $fields_string = http_build_query($fields);
-    
-        // open connection
-        $ch = curl_init();
-    
-        // set the option
-        curl_setopt($ch, CURLOPT_URL, $token_api_url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+            'postLink'        => $hbp_post_link,
+            'failurePostLink' => $hbp_failure_post_link
+          ];
         
-        $result = curl_exec($ch);
-        $json_result = json_decode($result, true);
-        if (!curl_errno($ch)) {
+          $fields_string = http_build_query($fields);
+        
+          $ch = curl_init();
+        
+          curl_setopt($ch, CURLOPT_URL, $token_api_url);
+          curl_setopt($ch, CURLOPT_POST, true);
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        
+          $result = curl_exec($ch);
+        
+          $json_result = json_decode($result, true);
+          if (!curl_errno($ch)) {
             switch ($http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
                 case 200:
-                    $hbp_access_token = $json_result["access_token"];
+                    $hbp_auth = (object) $json_result;
+        
+                    $hbp_payment_object = (object) [
+                        "invoiceId" => $hbp_invoice_id,
+                        "backLink" => $hbp_back_link,
+                        "failureBackLink" => $hbp_failure_back_link,
+                        "postLink" => $hbp_post_link,
+                        "failurePostLink" => $hbp_failure_post_link,
+                        "language" => $hbp_language,
+                        "description" => $hbp_description,
+                        "accountId" => $hbp_account_id,
+                        "terminal" => $hbp_terminal,
+                        "amount" => $hbp_amount,
+                        "currency" => $hbp_currency,
+                        "auth" => $hbp_auth,
+                        "phone" => $hbp_telephone,
+                        "email" => $hbp_email
+                    ];
                 ?>
-                    <script src="https://test-epay.homebank.kz/payform/payment-api.js"></script>
-                    <script>
-                        var createPaymentObject = function(auth, invoiceId, amount, currency) {
-                            var paymentObject = {
-                                invoiceId: invoiceId,
-                                backLink: "<?= $hbp_back_link ?>",
-                                failureBackLink: "<?= $hbp_failure_back_link ?>",
-                                postLink: "<?= $hbp_post_link ?>",
-                                failurePostLink: "<?= $hbp_failure_post_link ?>",
-                                language: "<?= $hbp_language ?>",
-                                description: "<?= $hbp_description ?>",
-                                accountId: "<?= $hbp_account_id ?>",
-                                terminal: "<?= $hbp_terminal ?>",
-                                amount: amount,
-                                currency: currency
-                            };
-                            paymentObject.auth = auth;
-                            return paymentObject;
-                        };
-                        halyk.pay(createPaymentObject(
-                            "<?= $hbp_access_token ?>", 
-                            "<?= $hbp_invoice_id ?>" , 
-                            "<?= $hbp_amount ?>", 
-                            "<?= $hbp_currency ?>"
-                            ));
-                     </script>
-                <?php
+                <script src="https://test-epay.homebank.kz/payform/payment-api.js"></script>
+                <script>
+                    halyk.pay(<?= json_encode($hbp_payment_object) ?>);
+                </script>
+            <?php
                     break;
                 default:
                     echo 'Неожиданный код HTTP: ', $http_code, "\n";
             }
         }
+        
     }
 }
 ?>
